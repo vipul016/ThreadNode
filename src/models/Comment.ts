d2fd2857ef7724d21284d9205a20ad01e98daa48
vipul016 <vipulwaskel934@gmail.com>
2026-02-20 12:32:36 +0530

    post model added

---
+++ src/models/Comment.ts
@@ -0,0 +1,51 @@
+import mongoose,{Schema,Document, mongo} from 'mongoose';
+
+export interface IComment extends Document{
+    content : string;
+    author : mongoose.Types.ObjectId;
+    post : mongoose.Types.ObjectId;
+    parentComment ?: mongoose.Types.ObjectId;
+    upvotes : number;
+    downvotes : number;
+    createdAt : Date;
+    updatedAt : Date;
+}
+
+const commentSchema : Schema<IComment> = new Schema({
+    content : {
+        type : String,
+        required : true,
+        maxLength : 10000
+    },
+    author : {
+        type : Schema.Types.ObjectId,
+        ref : 'User',
+        required : true,
+    },
+    post : {
+        type : Schema.Types.ObjectId,
+        ref: 'Post',
+        required : true,
+    },
+    parentComment : {
+        type : Schema.Types.ObjectId,
+        ref : 'Comment',
+        default : null,
+    },
+    upvotes : {
+        type : Number,
+        default : 0,
+    },
+    downvotes : {
+        type : Number,
+        default : 0,
+    }
+
+},{
+    timestamps : true,
+});
+
+commentSchema.index({post : 1, createdAt : 1});
+
+const Comment = mongoose.model<IComment>('Comment',commentSchema);
+export default Comment;
